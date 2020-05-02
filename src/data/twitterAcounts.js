72d1602100358929533dd37831a4196e9190ad13
Joanna Krezolek <j.krezolek@gmail.com>
2020-05-02 14:22:19 +0200

    create data and prepare images in assets

---
+++ src/data/twitterAcounts.js
@@ -0,0 +1,34 @@
+import DanAbramovImage from "../assets/images/DanAbramovImage";
+import RyanFlorenceImage from "../assets/images/RyanFlorenceImage";
+import MichaelJacksonImage from "../assets/images/MichaelJacksonImage";
+import MichaelCDoodsImage from "../assets/images/MichaelCDoods";
+
+export const twitterAcounts = [
+  {
+    image: DanAbramovImage,
+    name: "Dan Abramov",
+    description: "I didn’t make @reactjs",
+    twitterLink: "https://twitter.com/dan_abramov",
+  },
+  {
+    image: RyanFlorenceImage,
+    name: "Ryan Florence",
+    description:
+      "Making web development fun and awesome with @remix_run, @reacttraining, React Router, and Reach UI",
+    twitterLink: "https://twitter.com/ryanflorence",
+  },
+  {
+    image: MichaelJacksonImage,
+    name: "Michael Jackson",
+    description:
+      "Building @remix_run, @ReactTraining and @unpkg. Father of 2 sons, 2 daughters. Speaker, writer.",
+    twitterLink: "https://twitter.com/mjackson",
+  },
+  {
+    image: MichaelCDoodsImage,
+    name: "Michael C. Doods",
+    description:
+      "Improving the world w/ quality software · Husband, Father, Latter-day Saint, Teacher, OSS",
+    twitterLink: "https://twitter.com/kentcdodds",
+  },
+];
