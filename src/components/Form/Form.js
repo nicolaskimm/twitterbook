9d9a90e9ff79281c3374df34f4d4863b39f7bc7e
Joanna Krezolek <j.krezolek@gmail.com>
2020-05-03 21:10:54 +0200

    add styles to Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,13 +3,19 @@ import PropTypes from "prop-types";
 import "./Form.css";
 
 const Form = ({ addItem }) => (
-  <form onSubmit={addItem}>
-    <input placeholder="image" name="image" />
-    <input placeholder="name" name="name" />
-    <textarea placeholder="description" name="description" />
-    <input placeholder="link" name="link" />
-    <button type="submit"> add new item </button>
-  </form>
+  <div className="form__wrapper">
+    <h3> Add new twitter acccount </h3>
+    <form onSubmit={addItem} className="form__form">
+      <input placeholder="image" name="image" className="form__input" />
+      <input placeholder="name" name="name" className="form__input" />
+      <textarea placeholder="description" name="description" className="form__textarea" />
+      <input placeholder="link" name="link" className="form__input" />
+      <button type="submit" className="form__button">
+        {" "}
+        add new item{" "}
+      </button>
+    </form>
+  </div>
 );
 
 Form.propTypes = {
