4709fa424015ca98ad34d5e0e4b32ed101a78a2e
Joanna Krezolek <j.krezolek@gmail.com>
2020-05-04 15:57:17 +0200

    add simple validation to Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,16 +5,31 @@ import styles from "./Form.module.scss";
 const Form = ({ addItem }) => (
   <div className={styles.wrapper}>
     <h3> Add new twitter acccount </h3>
-    <form onSubmit={addItem} className={styles.form}>
+    <form onSubmit={addItem} className={styles.form} autoComplete="off">
       <input type="text" placeholder="image" name="image" id="image" className={styles.formInput} />
-      <input type="text" placeholder="name" name="name" id="name" className={styles.formInput} />
+      <input
+        type="text"
+        placeholder="name"
+        name="name"
+        id="name"
+        className={styles.formInput}
+        required
+      />
       <textarea
         placeholder="description"
         name="description"
         id="description"
         className={styles.formTextarea}
+        required
       />
-      <input type="text" placeholder="link" name="link" className={styles.formInput} id="link" />
+      <input
+        type="text"
+        placeholder="link"
+        name="link"
+        className={styles.formInput}
+        id="link"
+        required
+      />
       <button type="submit" className={styles.formButton}>
         add new item
       </button>
--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,18 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ListItem.module.scss";
 
-const ListItem = ({ image, name, description, link }) => (
-  <li className={styles.wrapper}>
-    <img src={image} alt={name} className={styles.image} />
-    <div>
-      <h2 className={styles.name}>{name}</h2>
-      <p className={styles.description}>{description}</p>
-      <a className={styles.button} href={link} target="_blank" rel="noopener noreferrer">
-        visit twitter page
-      </a>
-    </div>
-  </li>
-);
+const ListItem = ({ image, name, description, link }) => {
+  const ImageTag = image ? "img" : "div";
+
+  return (
+    <li className={styles.wrapper}>
+      <ImageTag
+        src={image}
+        alt={name}
+        className={ImageTag === "img" ? styles.image : styles.imageNone}
+      />
+      <div>
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.description}>{description}</p>
+        <a className={styles.button} href={link} target="_blank" rel="noopener noreferrer">
+          visit twitter page
+        </a>
+      </div>
+    </li>
+  );
+};
 
 ListItem.propTypes = {
   image: PropTypes.string.isRequired,
